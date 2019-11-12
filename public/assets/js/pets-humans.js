432d2793845b8bb73da27642261fb867ce3a6f70
Cassidy Jean Fuller <cassidyjean9@gmail.com>
2019-11-12 11:20:19 -0500

    adde #s

--- public/assets/js/pets-humans.js
+++ public/assets/js/pets-humans.js
@@ -2,13 +2,13 @@
 $(".human-form").on("submit", function (event) {
     event.preventDefault();
     var newHome = {
-        pet_type: $("question1-human").val(),
-        residence: $("question2-human").val(),
-        pet_size: $("question3-human").val(),
-        hypo: $("question4-human").val(),
-        activity: $("question5-human").val(),
-        pet_age: $("question6-human").val(),
-        has_kids: $("question7-human").val(),
+        pet_type: $("#question1-human").val(),
+        residence: $("#question2-human").val(),
+        pet_size: $("#question3-human").val(),
+        hypo: $("#question4-human").val(),
+        activity: $("#question5-human").val(),
+        pet_age: $("#question6-human").val(),
+        has_kids: $("#question7-human").val(),
     };
 
     $.ajax("/api/humans", {
@@ -27,17 +27,17 @@ $(".pet-form").on("submit", function (event) {
 
     console.log('pre-ajax');
     var newPet = {
-        pet_name: $("question1-pet").val(),
-        pet_type: $("question2-pet").val(),
-        pet_age: $("question3-pet").val(),
-        pet_breed: $("question4-pet").val(),
-        pet_size: $("question5-pet").val(),
-        hypo: $("question6-pet").val(),
-        activity: $("question7-pet").val(),
-        kid_friendly: $("question8-pet").val()
+        pet_name: $("#question1-pet").val(),
+        pet_type: $("#question2-pet").val(),
+        pet_age: $("#question3-pet").val(),
+        pet_breed: $("#question4-pet").val(),
+        pet_size: $("#question5-pet").val(),
+        hypo: $("#question6-pet").val(),
+        activity: $("#question7-pet").val(),
+        kid_friendly: $("#question8-pet").val()
     };
-if ($("question8-pet").val()) {
-    newPet.pet_picture = $("question9-pet").val().trim()
+if ($("#question9-pet").val()) {
+    newPet.pet_picture = $("#question9-pet").val().trim()
 }
 
     $.ajax("/api/pets", {
