2b057b7eb84411b3620a04d89220a445ea72a5ab
Charles Tuggle <tuggle.charles@gmail.com>
2019-11-12 12:11:14 -0500

    added matching functionality to apiRoutes.js

--- controller/apiRoutes.js
+++ controller/apiRoutes.js
@@ -1,5 +1,46 @@
 var db = require("../models");
 
+var matchingObject = {};
+var returningMatches = [];
+
+animalQueryingDB = function() {
+    matchingObject = req.body;
+    db.Humans.findAll({
+        where: {
+            pet_breed: matchingObject.pet_breed
+        }
+    });
+    returningMatches = [];
+    db.Humans.findAll({
+        where: {
+            pet_size: matchingObject.pet_size,
+            pet_type: matchingObject.pet_type,
+            residence: matchingObject.residence,
+            hypo: matchingObject.hypo,
+            activity: matchingObject.activity,
+            pet_age: matchingObject.pet_age
+        }
+    }); 
+};
+
+humanQueryingDB = function() {
+    matchingObject = req.body;
+    db.Pets.findAll({
+        where: {
+            pet_breed: matchingObject.pet_breed,
+        }
+    });
+    returningMatches = [];
+    db.Pets.findAll({
+        pet_size: matchingObject.pet_size,
+        pet_type: matchingObject.pet_type,
+        residence: matchingObject.residence,
+        hypo: matchingObject.hypo,
+        activity: matchingObject.activity,
+        pet_age: matchingObject.pet_age
+    });
+};
+
 module.exports = function(app) {
     // get all pets
     app.get("/api/pets", function(req, res) {
@@ -15,14 +56,16 @@ module.exports = function(app) {
     });
     // create new pet record
     app.post("/api/pets", function(req, res) {
-        db.Pets.create(req.body).then(function(response) {
-            res.json(response);
+        animalQueryingDB();
+        db.Pets.create(req.body).then(function(humanMatches) {
+            res.render(humanMatches);
         });
     });
     // create new human record
     app.post("/api/humans", function(req, res) {
-        db.Humans.create(req.body).then(function(response) {
-            res.json(response);
+        humanQueryingDB();
+        db.Humans.create(req.body).then(function(petMatches) {
+            res.render("results/partials/resultspets" ,{petMatches: petMatches});
         });
     });
 };
